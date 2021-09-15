f8b1736a53c3f7f18a4626e7a81bffc609429077
Luis <lupinha2014@gmail.com>
2021-09-15 17:21:40 -0300

    a

---
+++ react/site/src/service/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const api = axios.create({
+    baseURL: 'http://localhost:3030/'
+})
+
+export default class Api{
+    async listar() {
+        
+        let r = await api.get('/produto')
+        return r.data;
+    }
+
+    async inserir(nome, categoria, avaliacao, precode, precopor, estoque, linkimagem, descricao){
+
+        let r = await api.post('/produto', { nome, categoria, avaliacao, precode, precopor, estoque, linkimagem, descricao })
+        return r.data;
+    }
+
+    async alterar(id, nome, categoria, avaliacao, precode, precopor, estoque, linkimagem, descricao){
+        let r = await api.put('/produto/' + id, {nome, categoria, avaliacao, precode, precopor, estoque, linkimagem, descricao})
+        return r.data;
+    }
+    
+    async remover(id){
+        let r = await api.delete('/produto/' + id)
+        return r.data;
+    }
+
+}
